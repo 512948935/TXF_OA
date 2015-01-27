f207262a41f69a6bea431f2f32682eb475381186
TXF <cztxf_007@163.com>
2015-01-27 13:01:02 +0800

    aaaaa

--- Dao/DAL/tb_item_DepartmentDAL.cs
+++ Dao/DAL/tb_item_DepartmentDAL.cs
@@ -18,8 +18,16 @@ namespace Dao
                     where += " AND LEFT(ItemNo," + code.Length + ")='" + code + "'";
                 if (!string.IsNullOrEmpty(isDelete))
                     where += " AND IsDelete=0";
-                string sql = "SELECT COUNT(1)CNT FROM (SELECT a.ID,a.ItemNo,a.ItemName,b.ItemName Manager,a.Phone,a.Fax,a.IsDelete FROM dbo.tb_item_Department a"
-                           + "LEFT JOIN dbo.tb_item_User b ON b.ID=a.EmpID) AS t WHERE " + where + "";
+                string sql = string.Format(@"SELECT COUNT(1) CNT FROM ( SELECT    a.ID ,
+                                                                a.ItemNo ,
+                                                                a.ItemName ,
+                                                                b.ItemName Manager ,
+                                                                a.Phone ,
+                                                                a.Fax ,
+                                                                a.IsDelete
+                                                      FROM      dbo.tb_item_Department a
+                                                                LEFT JOIN dbo.tb_item_User b ON b.ID = a.EmpID
+                                                    ) AS t WHERE {0}) ", where);
                 total = (int)DBHelper.SingleQuery(sql);
                 sql = string.Format(@"SELECT TOP({0})* FROM(SELECT ROW_NUMBER()OVER(ORDER BY ItemNo)RowNo,* FROM 
                                 (SELECT a.ID,a.ItemID,a.ItemNo,a.ItemName,b.ItemName Manager,a.Phone,a.Fax,a.IsDelete 
